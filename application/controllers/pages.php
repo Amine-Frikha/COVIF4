573e8a4a87357949bd690cafe43ec7ebf9ccd5a3
Amine-Frikha <aminfrikha@outlook.com>
2021-03-25 18:46:38 +0100

    update

---
+++ application/controllers/pages.php
@@ -0,0 +1,15 @@
+<?php
+	class Pages extends CI_Controller{
+		public function view($page = 'acceuil'){
+			if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
+				show_404();
+			}
+			$data['title'] = 'Tableau de bord';
+			$this->load->view('templates/header',$data);
+			$this->load->view('pages/'.$page,$data);
+			$this->load->view('templates/footer');
+		}
+
+	}
+   
+?>
