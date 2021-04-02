2c36ec5cfb1645767672235948b9568da45cd25e
Amine-Frikha <aminfrikha@outlook.com>
2021-04-02 19:52:24 +0100

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,10 +6,10 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2020-2021 <a href="./acceuil">COVIF4</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0
+      <b>Version</b> 1.0 
     </div>
   </footer>
 </div>
