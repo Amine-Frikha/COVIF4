65c378e055170a12b4ae2975ebd200d21f526051
Amine-Frikha <aminfrikha@outlook.com>
2021-04-02 20:07:19 +0100

    Update login.php

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -27,7 +27,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition login-page" style="background-image: url('<?php echo base_url(); ?>assets/dist/img/sky.jpg'); background-size:cover;">
+<body class="hold-transition login-page" style="background-image: url('<?php echo base_url(); ?>assets/dist/img/stet.jpg'); background-size:cover;">
 <div class="login-box">
   <div class="login-logo">
     <a href="../../index2.html"><b>COV</b>IF4</a>
